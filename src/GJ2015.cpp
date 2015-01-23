8342c40b1a0158601bb09e40b31d9ed30bff4cf4
Marce Coll Rovira <coll.marce@gmail.com>
2015-01-24 00:18:31 +0100

    more useless commits

--- src/GJ2015.cpp
+++ src/GJ2015.cpp
@@ -63,3 +63,31 @@ void GJ2015::processEvents(){
 sf::RenderWindow* GJ2015::getWindow() {
     return &window;
 }
+
+GJ2015::ObjectJSONHandler {
+        bool Null() {return true;};
+        bool Int(int i) { cout << "Int(" << i << ")" << endl; return true; }
+        bool Uint(unsigned u) { cout << "Uint(" << u << ")" << endl; return true; }
+        bool Int64(int64_t i) { cout << "Int64(" << i << ")" << endl; return true; }
+        bool Uint64(uint64_t u) { cout << "Uint64(" << u << ")" << endl; return true; }
+
+        // JSON Basic Types
+        bool Bool(bool b) { cout << "Bool(" << boolalpha << b << ")" << endl; return true; }
+        bool Double(double d) { cout << "Double(" << d << ")" << endl; return true; }
+        bool String(const char* str, SizeType length, bool copy) { 
+            cout << str << endl;
+            return true;
+        }
+
+        // JSON Object
+        bool StartObject() { cout << "{" << endl; return true; }
+        bool Key(const char* str, SizeType length, bool copy) {
+            cout << str << ": ";
+            return true;
+        }
+        bool EndObject(SizeType memberCount) { cout << "}" << endl; return true; }
+
+        // JSON Array
+        bool StartArray() { cout << "[" << endl; return true; }
+        bool EndArray(SizeType elementCount) { cout << "]" << endl; return true; }
+    };
