e51cea5473598a496636a8ee8985a9b2c3a07fd4
Marce Coll Rovira <coll.marce@gmail.com>
2015-01-23 21:17:58 +0100

    Added project

---
+++ src/GJ2015.h
@@ -0,0 +1,25 @@
+#ifndef MYGAME_H
+#define MYGAME_H
+#include "Game.hpp"
+#include "Object.h"
+
+class Object;
+class GJ2015 : public Game {
+public:
+    GJ2015(int scrwidth, int scrheight, std::string title, int style);
+    ~GJ2015();
+
+    void init();
+    void update(float deltaTime);
+    void draw();
+    
+    void processEvents();
+    void keyPressed(sf::Event event);
+    void keyReleased(sf::Event event);    sf::RenderWindow* getWindow();
+private:
+    std::list<Object*> objects;
+
+    std::vector<std::vector<dir>> isKeyPressed;
+};
+
+#endif // MYGAME_H
