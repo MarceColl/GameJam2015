165976c9ecf3447358b12dcdc7c3740c4bb04e35
Marce Coll Rovira <coll.marce@gmail.com>
2015-01-23 22:13:26 +0100

    Modifed some shiet

--- src/Object.cpp
+++ src/Object.cpp
@@ -2,13 +2,13 @@
 #define OBJECT_HPP
 #include "utils.hpp"
 
-class LD48;
+class GJ2015;
 class Object
 {
 public:
-    Object(LD48* game, sf::Vector2f size, sf::Texture& tex, sf::Vector2i spriteCount );
+    Object(GJ2015* game, sf::Vector2f size, sf::Texture& tex, sf::Vector2i spriteCount );
 
-    LD48* game;
+    GJ2015* game;
 
     sf::Vector2f position;
     sf::Vector2f size;
