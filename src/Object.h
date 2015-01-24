a197dbcdabeee3d41a3c6d0d0d063982c844dc22
Marce Coll Rovira <coll.marce@gmail.com>
2015-01-24 03:46:26 +0100

    DO I LOOK LIKE A HOUSEMAID?

--- src/Object.h
+++ src/Object.h
@@ -39,44 +39,34 @@ public:
     sf::Sprite sprite;
 
     Size objectSize;
+
     bool isContainer;
     bool isMovable;
+    bool inObject;
+    
+    std::string locationInObject;
+    std::string objectName;
+
+    std::vector<std::string> initStates;
+    std::vector<std::vector<std::string> > possibleStates;
+
+    std::map<std::string, DoesInteract> interactionsAvailable;
+    std::map<std::string, RecievesInteract> interactionsRecievable;
 
     void draw();
 
     void actsOn(Object* b);
-	
-    std::string objectName;
-
-    bool inObject;
-    std::string locationInObject;
-
-    std::vector<std::string> initStates;
-	
-    std::vector<std::vector<std::string> > possibleStates;
-
-    std::map<std::string, DoesInteract> interactionsAvailable;
-
-    std::map<std::string, RecievesInteract> interactionsRecievable;
 
 	void setPosition(sf::Vector2f pos);
-
     void setObjectName(std::string nom);
-
     void setLocationInObject(std::string obj);
-
     void setObjectSize(Size s);
-
     void setIsContainer(bool isCont);
-
     void setIsMovable(bool isMov);
-
     void setInitStates(std::vector<std::string> ei);
-	
     void setPossibleStates(std::vector<std::vector<std::string> > ps);
 
     void addInteraction(DoesInteract di);
-
     void addInteraction(RecievesInteract ri);
 	
 	bool canInteractWithAction(std::string act);
